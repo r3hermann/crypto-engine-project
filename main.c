f0fce217775c0013b62b3c48d6c53024b2562120
Hermann <ecalafiore@virgilio.it>
2020-04-15 23:56:26 +0200

    main

--- main.c
+++ main.c
@@ -23,6 +23,8 @@
 
 int main (int argc, char* argv[]){
     
+    long fixed_msg = 0;
+    
     //time
     elapsed_time_t time;
     stats_t timing;
@@ -36,34 +38,50 @@ int main (int argc, char* argv[]){
 
     state_t alice_state, bob_state, proxy_state;
     msg_t a2b_msg, b2a_msg, a2p_msg;
+    plaintext_t plaintext_msg;
 
-    //shared params
+    //shared params N
     shared_params_t params;
     
     public_key_t pk;
     private_key_t sk;
     weak_secret_key_t wsk;
     
-    //hashing_t hashfnc;
-    
-    
     int exit_status=0;
     
     long prng_seed=random_seed();
     //printf("random_seed %ld\n",seed);
+    
+    if (*argv[1]=='\0') {
+        printf("esecuzione minimale...\n\n");
+    }
     
 	for(int i=1; i<argc; i++){
         
          if (strcmp(argv[i], "verbose") == 0)
             set_messaging_level(msg_very_verbose);
          
-          else if (strcmp(argv[i], "bench") == 0) {
-              
+         else if (strcmp(argv[i], "bench") == 0) {
+             
             applied_sampling_time = bench_sampling_time;
             do_bench =true;
-        }else {
-            printf("utilizzo: %s [verbose] [bench]\n",
-                basename(argv[0]));
+             
+        } else if (strcmp(argv[i], "message") == 0) {
+            
+            
+            if(i+1 >= argc) {
+                printf("error: \"message\" errato o mancante \n");
+                exit(1);
+            }
+            assert(argv[i+1]);
+            fixed_msg = atoi(argv[i+1]);
+            i++;
+        }
+        
+        else {
+            printf("utilizzo eseguibile: ./%s [verbose] [message <n> ]"
+                     "[bench]\nusare il comando ./%s per un esecuzione minimale\n",
+                basename(argv[0]), basename(argv[0]));
             exit(1);
         }
         //...
@@ -96,10 +114,10 @@ int main (int argc, char* argv[]){
         gmp_randseed_os_rng(prng, prng_sec_level);
     }
     
+    //get N=p*q
     printf("\nGenerazione dei parametri comuni. Gruppo di ordine %d bit...\n",p_bits);
-    
     perform_oneshot_timestamp_sampling(time, tu_sec, {
-        generate_shared_params(params,p_bits,prng);
+        generate_shared_params(params, p_bits, prng);
         });
     if (do_bench)
         printf_et("generate_shared_params: ", time, tu_sec,"\n");
@@ -122,9 +140,28 @@ int main (int argc, char* argv[]){
     
     //alice= a, g^a
     printf("Generazione parametri di Alice\n");
-   generate_keys(pk, sk, wsk, a2p_msg,alice_state,params,prng);
+    generate_keys(pk, sk, wsk, a2p_msg,alice_state,params,prng);
     
-    encrypt(params,prng );
+    plaintext_init(plaintext_msg);
+    if (fixed_msg > 0) {
+        mpz_set_ui(plaintext_msg->m,fixed_msg);
+    }
+    else {
+        if (fixed_msg < 0)
+            mpz_sub_ui(plaintext_msg->m, params->N, labs(fixed_msg));
+        else
+            mpz_urandomm(plaintext_msg->m, prng, params->N);
+    }
+    //gmp_printf("check plaintext->m: %Zd\n", plaintext_msg->m);
+    //gmp_printf("check N: %Zd\n", params->N);
+
+   printf("\nCifratura...\n");
+   perform_clock_cycles_sampling_period(
+       timing, applied_sampling_time, max_samples, tu_millis,{
+           encrypt(params, prng, plaintext_msg,pk);},{});
+   if (do_bench)
+            printf_short_stats(" rsa_encryption", timing, "");
+   
     /*
     //bob= b, g^b
     printf("Generazione parametri di Bob\n");
@@ -156,6 +193,7 @@ int main (int argc, char* argv[]){
     private_key_clear(sk);
     weak_secret_key_clear(wsk);
     shared_params_clear(params);
+    plaintext_clear(plaintext_msg);
     gmp_randclear(prng);
     exit(exit_status);
 }
